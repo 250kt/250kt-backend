0961dc31cc546726807f9b086d667be16ba4d4a1
valentincp80190 <valentincp80190@gmail.com>
2023-05-05 19:31:19 +0200

    feat: add three modules (gofly-api, gofly-batch-partition-airspace-data and gofly-commons)

---
+++ gofly-api/src/main/java/fr/gofly/GoflyApiApplication.java
@@ -0,0 +1,13 @@
+package fr.gofly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoflyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoflyApiApplication.class, args);
+    }
+
+}
---
+++ gofly-batch-partition-airspace-data/src/main/java/fr/gofly/GoflyBatchPartitionAirspaceDataApplication.java
@@ -0,0 +1,13 @@
+package fr.gofly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoflyBatchPartitionAirspaceDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoflyBatchPartitionAirspaceDataApplication.class, args);
+    }
+
+}
