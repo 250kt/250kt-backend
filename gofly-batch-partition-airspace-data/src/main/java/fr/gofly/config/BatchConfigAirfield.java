640bebd43754035e59168fdf32426dcaf26a673b
valentincp80190 <valentincp80190@gmail.com>
2023-05-11 22:16:04 +0200

    feat: WIP add writer to Airfield

--- gofly-batch-partition-airspace-data/src/main/java/fr/gofly/config/BatchConfigAirfield.java
+++ gofly-batch-partition-airspace-data/src/main/java/fr/gofly/config/BatchConfigAirfield.java
@@ -3,11 +3,13 @@ package fr.gofly.config;
 import fr.gofly.model.Airfield;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +19,17 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableBatchProcessing
 public class BatchConfigAirfield {
     @Autowired
     private PlatformTransactionManager transactionManager;
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     public Job job(JobRepository jobRepository, Step step){
@@ -45,8 +51,8 @@ public class BatchConfigAirfield {
     public StaxEventItemReader<Airfield> itemReader() {
         return new StaxEventItemReaderBuilder<Airfield>()
                 .name("itemReader")
-                .resource(new FileSystemResource("files/import.xml"))
-                .addFragmentRootElements("As")
+                .resource(new FileSystemResource("gofly-batch-partition-airspace-data/files/import.xml"))
+                .addFragmentRootElements("Ad")
                 .unmarshaller(airfieldMarshaller())
                 .build();
     }
@@ -55,9 +61,31 @@ public class BatchConfigAirfield {
         XStreamMarshaller marshaller = new XStreamMarshaller();
 
         Map<String, String> alias = new HashMap<>();
-        alias.put("Airfield", "fr.gofly.model.Airfield");
+        alias.put("Ad", "fr.gofly.model.Airfield");
 
         marshaller.setAliases(alias);
         return marshaller;
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Airfield> writer(){
+        JdbcBatchItemWriter<Airfield> writer = new JdbcBatchItemWriter<Airfield>();
+        writer.setDataSource(dataSource);
+        writer.setSql(
+                "insert into airfield " +
+                        "(airfield_id, " +
+                        "airfield_accept_vfr, " +
+                        "airfield_fullname, " +
+                        "airfield_map_name, " +
+                        "airfield_phone, " +
+                        "airfield_situation, " +
+                        "airfield_statut, " +
+                        "airfield_altitude, " +
+                        "airfield_latitude_arp, " +
+                        "airfield_longitude_arp) " +
+                        "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        );
+        writer.setItemPreparedStatementSetter(new AirfieldItemPreparedStatementSetter());
+        return writer;
     }
 }
--- gofly-commons/src/main/java/fr/gofly/model/Airfield.java
+++ gofly-commons/src/main/java/fr/gofly/model/Airfield.java
@@ -1,6 +1,7 @@
 package fr.gofly.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
@@ -10,42 +11,52 @@ import lombok.RequiredArgsConstructor;
 public class Airfield {
     @Id
     @GeneratedValue
+    @Getter
     @Column(name="airfield_id")
     private String airfieldId;
 
+    @Getter
     @Column(name="airfield_statut",
             nullable = false)
     private String airfieldStatut;
 
+    @Getter
     @Column(name="airfield_fullname",
             nullable = false)
     private String airfieldFullname;
 
+    @Getter
     @Column(name="airfield_map_name",
             nullable = true)
     private String airfieldMapName;
 
+    @Getter
     @Column(name="airfield_situation",
             nullable = false)
     private String airfieldSituation;
 
-    @Column(name="airfield_phone",
+    @Getter
+    @Column(name="airfield_phone_number",
             nullable = false)
-    private String airfieldPhone;
+    private String airfieldPhoneNumber;
 
+    @Getter
     @Column(name="airfield_altitude",
             nullable = false)
     private int airfield_altitude;
 
+    @Getter
     @Column(name="airfield_accept_Vfr",
             nullable = false)
     private boolean airfieldAcceptVfr;
 
-    @Column(name="latitude_ARP",
+    @Getter
+    @Column(name="airfield_latitude_ARP",
             nullable = false)
-    private float latitudeARP;
+    private float airfieldLatitudeARP;
 
-    @Column(name="longitude_ARP",
+    @Getter
+    @Column(name="airfield_longitude_ARP",
             nullable = false)
-    private float longitudeARP;
+    private float airfieldLongitudeARP;
 }
