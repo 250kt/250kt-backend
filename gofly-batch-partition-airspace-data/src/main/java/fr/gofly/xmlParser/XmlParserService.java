0aaf91fd35978b7bb05536f18b0cd3be05eda7ea
valentincp80190 <valentincp80190@gmail.com>
2023-08-09 10:36:17 +0200

    Refactor XmlParserService

--- gofly-batch-partition-airspace-data/src/main/java/fr/gofly/xmlParser/XmlParserService.java
+++ gofly-batch-partition-airspace-data/src/main/java/fr/gofly/xmlParser/XmlParserService.java
@@ -3,6 +3,7 @@ package fr.gofly.xmlParser;
 import fr.gofly.model.Airfield;
 import fr.gofly.model.SiaExport;
 import fr.gofly.repository.AirfieldRepository;
+import fr.gofly.xmlParser.export.AirfieldExportService;
 import jakarta.annotation.PostConstruct;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Unmarshaller;
@@ -17,21 +18,24 @@ import java.io.File;
 public class XmlParserService {
 
     private final String xmlFilePath = "import.xml";
+    private Logger logger = LoggerFactory.getLogger(XmlParserService.class);
+    private SiaExport siaExport;
 
     @Autowired
     private AirfieldRepository airfieldRepository;
 
-    private Logger logger = LoggerFactory.getLogger(XmlParserService.class);
+    @Autowired
+    private AirfieldExportService airfieldExportService;
 
-    private SiaExport siaExport;
+
 
     /**
      * Parses the XML file and exports airfields to the database.
      * This method is automatically executed after the Spring bean is constructed.
      */
     @PostConstruct
-    public void parseXMLFile(){
-        try{
+    public void parseXMLFile() {
+        try {
             logger.info("XML Parser : STARTED");
 
             File xmlFile = ResourceUtils.getFile("classpath:" + xmlFilePath);
@@ -41,57 +45,13 @@ public class XmlParserService {
 
             siaExport = (SiaExport) jaxbUnmarshaller.unmarshal(xmlFile);
 
-            exportAirfieldsToDatabase();
+            airfieldExportService.exportAirfieldsToDatabase(siaExport);
 
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("An error occurred during the process : " + e.getMessage());
             throw new RuntimeException("An error occurred during the process : " + e.getMessage(), e);
         } finally {
             logger.info("XML Parser : FINISHED");
-        }
-    }
-
-    /**
-     * Exports airfields to the database based on the parsed XML data.
-     * This method is called internally by parseXMLFile().
-     */
-    private void exportAirfieldsToDatabase() {
-        try{
-            logger.info("Airfields export : STARTED");
-            if (siaExport != null && siaExport.getAirfields() != null) {
-                logger.info("Airfields export : " + siaExport.getAirfields().size() + " airfields found");
-
-                for (Airfield airfield: siaExport.getAirfields()) {
-                    saveAirfieldToDatabase(airfield);
-                }
-
-                logger.info("Airfields export : " + airfieldRepository.countBy() + " airfields inserted");
-                if(siaExport.getAirfields().size() != airfieldRepository.countBy()){
-                    logger.warn("Airfields export : Not all airfields have been exported to the database");
-                }
-            }else{
-                logger.warn("Airfields export : No airfields found in the XML file");
-            }
-        }catch (Exception e){
-            logger.error("Error during export of Airfield to database: " + e.getMessage());
-            throw new RuntimeException("Error during export of Airfield to database: " + e.getMessage(), e);
-        }finally {
-            logger.info("Airfields export : FINISHED");
-        }
-    }
-
-    /**
-     * Saves an airfield to the database.
-     *
-     * @param airfield The airfield object to be saved.
-     * @throws RuntimeException If an error occurs during the database operation.
-     */
-    private void saveAirfieldToDatabase(Airfield airfield) {
-        try {
-            airfieldRepository.save(airfield);
-        } catch (Exception e) {
-            logger.error("Error saving Airfield to database: " + e.getMessage());
-            throw new RuntimeException("Error saving Airfield to database: " + e.getMessage(), e);
         }
     }
 }
