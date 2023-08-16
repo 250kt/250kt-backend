f9d27d7f5e367b0a9d3d498b06662ba8b1ccb160
valentincp80190 <valentincp80190@gmail.com>
2023-08-16 14:34:34 +0200

    Add h2 database to API project tests

--- gofly-api/src/test/java/fr/gofly/GoflyApiApplicationTests.java
+++ gofly-api/src/test/java/fr/gofly/GoflyApiApplicationTests.java
@@ -2,9 +2,13 @@ package fr.gofly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class GoflyApiApplicationTests {
+@SpringBootTest(classes = {
+        GoflyApiApplication.class,
+        H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
+class GoflyExportDataApplicationTests {
 
     @Test
     void contextLoads() {
