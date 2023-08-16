4c9497419a6b506bd2e5620ecd5683e40feef9cc
valentincp80190 <valentincp80190@gmail.com>
2023-08-16 14:27:18 +0200

    Add h2 database

--- gofly-export-data/src/test/java/fr/gofly/GoflyExportDataApplicationTests.java
+++ gofly-export-data/src/test/java/fr/gofly/GoflyExportDataApplicationTests.java
@@ -2,8 +2,12 @@ package fr.gofly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = {
+        GoflyExportData.class,
+        H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
 class GoflyExportDataApplicationTests {
 
     @Test
