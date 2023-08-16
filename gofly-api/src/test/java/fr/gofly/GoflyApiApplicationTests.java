e385d71878d80229fe36c3b24f1dd2ba49393a54
valentincp80190 <valentincp80190@gmail.com>
2023-08-16 17:28:26 +0200

    Add user model and update some project properties

--- gofly-api/src/test/java/fr/gofly/GoflyApiApplicationTests.java
+++ gofly-api/src/test/java/fr/gofly/GoflyApiApplicationTests.java
@@ -2,14 +2,9 @@ package fr.gofly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = {
-        GoflyApiApplication.class,
-        H2TestProfileJPAConfig.class})
-@ActiveProfiles("test")
-class GoflyExportDataApplicationTests {
-
+@SpringBootTest
+class GoflyApiApplicationTests {
     @Test
     void contextLoads() {
     }
