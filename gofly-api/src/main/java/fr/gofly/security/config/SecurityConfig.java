22ed22a00ff7a4984b20def386b94e7f4416b1c1
valentincp80190 <valentincp80190@gmail.com>
2023-10-11 21:54:32 +0200

    Add how to put autority requirement to an endpoint.

--- gofly-api/src/main/java/fr/gofly/security/config/SecurityConfig.java
+++ gofly-api/src/main/java/fr/gofly/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package fr.gofly.security.config;
 
+import fr.gofly.model.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,22 +32,21 @@ public class SecurityConfig {
         http
                 .csrf().disable() // Disable Cross-Site Request Forgery (CSRF)
                 .authorizeHttpRequests()
-                //Everybody are able to access to the application patterns bellow
-                .requestMatchers("/api/auth/**")
-                .permitAll()
-                //For all the others, everybody need to be authenticated with a JWT token
-                .anyRequest()
-                .authenticated()
+                    //Everybody are able to access to the application patterns bellow
+                    .requestMatchers("/api/auth/**").permitAll()
+                    //For all the others, everybody need to be authenticated with a JWT token
+                    .requestMatchers("/api/home").hasAuthority(Role.BUDDING_PILOT.toString())
+                    .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout()
-                .logoutUrl("/api/auth/logout")
-                .addLogoutHandler(logoutHandler)
-                .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
+                    .logoutUrl("/api/auth/logout")
+                    .addLogoutHandler(logoutHandler)
+                    .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
 
         return http.build();
     }
