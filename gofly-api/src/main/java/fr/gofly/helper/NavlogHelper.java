84d15d5e823ce0c31cd2dd04059420d364331109
valentincp80190 <valentincp80190@gmail.com>
2023-10-15 21:13:37 +0200

    Add commentaries for documentation.

--- gofly-api/src/main/java/fr/gofly/helper/NavlogHelper.java
+++ gofly-api/src/main/java/fr/gofly/helper/NavlogHelper.java
@@ -7,14 +7,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+/**
+ * Helper class for validating and processing navigation logs (Navlogs).
+ */
 @Component
 @RequiredArgsConstructor
 public class NavlogHelper {
 
+    /**
+     * Check if a navigation log (Navlog) is owned by a specific user.
+     *
+     * @param navlog The Navlog to check.
+     * @param user The user to compare ownership.
+     * @return True if the Navlog is owned by the user, false otherwise.
+     */
     public boolean isNavlogOwnedByUser(Navlog navlog, User user){
         return Objects.equals(navlog.getUser().getId(), user.getId());
     }
 
+    /**
+     * Check if a navigation log (Navlog) has all mandatory fields filled.
+     *
+     * @param navlog The Navlog to check.
+     * @return True if the Navlog contains all mandatory fields, false otherwise.
+     */
     public boolean isMissingMandatoryFields(Navlog navlog){
         int steps = navlog.getSteps().size();
 
--- gofly-api/src/main/java/fr/gofly/service/NavlogService.java
+++ gofly-api/src/main/java/fr/gofly/service/NavlogService.java
@@ -5,7 +5,6 @@ import fr.gofly.helper.UserHelper;
 import fr.gofly.model.Navlog;
 import fr.gofly.model.User;
 import fr.gofly.repository.NavlogRepository;
-import fr.gofly.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +12,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * Service class for managing navigation logs (Navlogs).
+ */
 @Service
 @RequiredArgsConstructor
 public class NavlogService {
@@ -20,6 +22,12 @@ public class NavlogService {
     private final NavlogHelper navlogHelper;
     private final UserHelper userHelper;
 
+    /**
+     * Create a new navigation log (Navlog).
+     *
+     * @param navlog The Navlog to be created.
+     * @return An Optional containing the created Navlog, or empty if mandatory fields are missing.
+     */
     public Optional<Navlog> createNavlog(Navlog navlog){
         if(navlogHelper.isMissingMandatoryFields(navlog))
             return Optional.empty();
@@ -27,6 +35,13 @@ public class NavlogService {
         return Optional.of(navlogRepository.save(navlog));
     }
 
+    /**
+     * Retrieve a specific navigation log (Navlog) based on its ID.
+     *
+     * @param navlogId The ID of the Navlog to retrieve.
+     * @param user The user requesting the retrieval.
+     * @return An Optional containing the retrieved Navlog, or empty if it doesn't exist, or if the user doesn't have the necessary permissions.
+     */
     public Optional<Navlog> retrieveNavlog(long navlogId, User user){
         Optional<Navlog> navlogOptional = navlogRepository.findById(navlogId);
 
@@ -39,14 +54,32 @@ public class NavlogService {
         return navlogOptional;
     }
 
+    /**
+     * Retrieve all navigation logs (Navlogs) for a specific user.
+     *
+     * @param user The user for whom to retrieve Navlogs.
+     * @return A set of Navlogs associated with the user.
+     */
     public Set<Navlog> retrieveNavlogs(User user){
         return navlogRepository.findAllByUserId(user.getId());
     }
 
+    /**
+     * Retrieve all navigation logs (Navlogs).
+     *
+     * @return A list of all available Navlogs.
+     */
     public List<Navlog> retrieveAllNavlogs(){
         return navlogRepository.findAll();
     }
 
+    /**
+     * Delete a specific navigation log (Navlog).
+     *
+     * @param navlog The Navlog to be deleted.
+     * @param user The user requesting the deletion.
+     * @return True if the deletion was successful, false otherwise (e.g., missing permissions).
+     */
     public boolean deleteNavlog(Navlog navlog, User user){
         if(!navlogHelper.isNavlogOwnedByUser(navlog, user) || !userHelper.isAdmin(user))
             return false;
@@ -55,6 +88,13 @@ public class NavlogService {
         return true;
     }
 
+    /**
+     * Update an existing navigation log (Navlog).
+     *
+     * @param navlog The Navlog to be updated.
+     * @param user The user requesting the update.
+     * @return An Optional containing the updated Navlog, or empty if mandatory fields are missing, or if the user doesn't have the necessary permissions.
+     */
     public Optional<Navlog> updateNavlog(Navlog navlog, User user){
         if(navlogHelper.isMissingMandatoryFields(navlog) ||
            !navlogHelper.isNavlogOwnedByUser(navlog, user) ||
