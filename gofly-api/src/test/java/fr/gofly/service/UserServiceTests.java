13757da78b451a9cfa0e044024952eadb9d552c4
augustinde <augustin.1201@outlook.fr>
2024-08-04 15:34:38 +0200

    feat: adding tests

--- gofly-api/src/test/java/fr/gofly/service/UserServiceTests.java
+++ gofly-api/src/test/java/fr/gofly/service/UserServiceTests.java
@@ -11,10 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -33,11 +37,24 @@ public class UserServiceTests {
     @Mock
     private UserToUserDto userMapper;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
     private List<Authority> authorities = new ArrayList<>();
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    private User user;
+    private String fromEmail;
 
     @BeforeEach
     void setUp(){
         authorities.add(Authority.BUDDING_PILOT);
+        user = new User();
+        user.setEmail("test@example.com");
+        user.setVerificationCode("verificationCode");
+        fromEmail = "fromEmail";
     }
 
     @Test
@@ -94,5 +111,32 @@ public class UserServiceTests {
         when(userMapper.map(any(User.class))).thenReturn(new UserDto());
 
         assertEquals(Optional.of(userMapper.map(user)), userService.updateUser(user));
+    }
+
+
+    public boolean confirmEmail(String code){
+        Optional<User> userOptional = userRepository.findByVerificationCodeAndIsEmailConfirmedFalse(code);
+        if(userOptional.isPresent()){
+            User user = userOptional.get();
+            user.setIsEmailConfirmed(true);
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean changePassword(User user, String newPassword){
+        if(Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$", newPassword)){
+            user.setPassword(passwordEncoder.encode(newPassword));
+            userRepository.save(user);
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(user.getEmail());
+            mailMessage.setFrom(fromEmail);
+            mailMessage.setSubject("Password changed");
+            mailMessage.setText("Your password has been changed.");
+            javaMailSender.send(mailMessage);
+            return true;
+        }
+        return false;
     }
 }
